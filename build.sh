b049046fad2e1ff0293b4feb487f8f266436ed05
himitnarayan <himitnarayan31@gmail.com>
2026-02-02 20:37:03 +0530

    kaggle dataset build pipeline

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
+pip install kaggle
+
+echo "Setting up Kaggle credentials..."
+mkdir -p ~/.kaggle
+echo "$KAGGLE_JSON" > ~/.kaggle/kaggle.json
+chmod 600 ~/.kaggle/kaggle.json
+
+echo "Downloading TMDB dataset from Kaggle..."
+mkdir -p recommender/data
+kaggle datasets download -d asaniczka/tmdb-movies-dataset-2023-930k-movies -p recommender/data --unzip
 
 echo "Building ML model..."
 python build_model.py
