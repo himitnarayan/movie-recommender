a4fe628f54dc32d7ebf9c3cdd45399fea9c8e1a4
himitnarayan <himitnarayan31@gmail.com>
2026-05-14 13:14:59 +0530

    fix: remove heavy data processing from build script

--- build.sh
+++ build.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
+# exit on error
+set -o errexit
 
+echo "Installing requirements..."
 pip install -r requirements.txt
-pip install kaggle
 
-echo "Setting up Kaggle credentials..."
-mkdir -p ~/.kaggle
-echo "$KAGGLE_JSON" > ~/.kaggle/kaggle.json
-chmod 600 ~/.kaggle/kaggle.json
-
-echo "Downloading TMDB dataset from Kaggle..."
-mkdir -p recommender/data
-kaggle datasets download -d asaniczka/tmdb-movies-dataset-2023-930k-movies -p recommender/data --unzip
-
-echo "Building ML model..."
-python build_model.py
-
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
